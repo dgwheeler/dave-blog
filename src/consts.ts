013d9daa64729e02531f83e1fa790fb9766bd6fd
dgwheeler <dave.wheeler@outlook.com>
2026-03-17 18:53:55 -0700

    Update site config

--- src/consts.ts
+++ src/consts.ts
@@ -2,4 +2,4 @@
 // You can import this data from anywhere in your site by using the `import` keyword.
 
 export const SITE_TITLE = 'Midnight Blogger';
-export const SITE_DESCRIPTION = 'Blog from midnight to dawn.';
+export const SITE_DESCRIPTION = 'From midnight to dawn';
